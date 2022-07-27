5ed1b443faca4a3517471971e8dbb58ab390d8b4
jcastellanos <jcastellanos@zetti.com.ar>
2022-07-27 00:52:25 -0300

    added psvm

--- 8kyu/java/Multiply.java/src/Multiply.java
+++ 8kyu/java/Multiply.java/src/Multiply.java
@@ -1,9 +1,10 @@
 //DESCRIPTION:
 //This code does not execute properly. Try to figure out why.
 
-
-
 public class Multiply {
+    public static void main(String[]args){
+        double result = multiply(6.6,3.2);
+    }
     public static double multiply(Double a, Double b) {
         return a * b;
     }
