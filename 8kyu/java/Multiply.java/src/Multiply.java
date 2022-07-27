baebd2516a4c0eed20debe3b1b941b8444b061bc
jcastellanos <jcastellanos@zetti.com.ar>
2022-07-27 00:48:56 -0300

    challenge easy java init

---
+++ 8kyu/java/Multiply.java/src/Multiply.java
@@ -0,0 +1,10 @@
+//DESCRIPTION:
+//This code does not execute properly. Try to figure out why.
+
+
+
+public class Multiply {
+    public static double multiply(Double a, Double b) {
+        return a * b;
+    }
+}
